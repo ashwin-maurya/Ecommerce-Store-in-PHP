ee91fdacb40457e370d4084ff52749a150200bec
Ashwin Maurya <ashwinmaurya30@gmail.com>
2023-09-06 12:49:08 +0530

    deleted all

--- web/partials/_dbconnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Script to connect to the database
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "shopping";
-$conn = mysqli_connect($servername, $username, $password, $database);
